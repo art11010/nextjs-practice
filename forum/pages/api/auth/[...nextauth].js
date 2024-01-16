776ba93dd2e0140c3c5cbbc8a9dcb95bc96a807b
YEZI <yezi.dev@gmail.com>
2024-01-16 12:00:00 +0900

    add nextauth jwt secret key

--- forum/pages/api/auth/[...nextauth].js
+++ forum/pages/api/auth/[...nextauth].js
@@ -10,6 +10,7 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  secret: 'qwer1234',
 };
 
 export default NextAuth(authOptions);
@@ -21,5 +22,5 @@ export default NextAuth(authOptions);
 //       clientSecret: 'Github에서 발급받은Secret',
 //     }),
 //   ],
-//   secret: 'jwt 생성 시 쓰는 암호',  // 소셜 로그인은 JWT 방식 사용
+//   secret: 'jwt 생성 시 쓰는 암호',  // 소셜 로그인은 기본적으로 JWT 방식 사용
 // };
