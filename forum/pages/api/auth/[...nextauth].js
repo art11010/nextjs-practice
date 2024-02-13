fd2e4d6d193373bfd7ed17dd8bb37934c51945d7
YEZI <yezi.dev@gmail.com>
2024-02-13 12:00:00 +0900

    DB Adapter comments

--- forum/pages/api/auth/[...nextauth].js
+++ forum/pages/api/auth/[...nextauth].js
@@ -27,3 +27,8 @@ export default NextAuth(authOptions);
 //   ],
 //   secret: 'jwt 생성 시 쓰는 암호',  // 소셜 로그인은 기본적으로 JWT 방식 사용
 // };
+
+// MongoDBAdapter를 사용하여 로그인 시 MongoDB에 3개의 컬렉션이 생성된다
+// accounts : 가입된 유저 계정 보관
+// sessions : 현재 로그인 된 유저 정보(+ 로그인 유효기간)
+// users : 가입된 유저 정보 보관(유저 간 구분은 이메일로 함)
