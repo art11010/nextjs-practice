acb94fda7ddd19382ef9bb1460f9ae9050c5797c
YEZI <yezi.dev@gmail.com>
2024-02-23 12:00:00 +0900

    add unauthorized process

--- forum/app/detail/[id]/Buttons.js
+++ forum/app/detail/[id]/Buttons.js
@@ -5,39 +5,39 @@ import Link from 'next/link';
 export default function Buttons({ id }) {
   return (
     <div className="flex justify-end gap-4">
-      <button
+      {/* <button
         onClick={() => {
           fetch('/api/test?name=lee&age=20');
           // fetch('/api/test/lee');
         }}
       >
         데이터 전송
-      </button>
-      {/* <button
+      </button> */}
+      <button
         className="text-sm text-gray-500"
-        onClick={() => {
-          fetch('/api/post/delete', {
-            method: 'DELETE',
-            body: JSON.stringify({ id }),
-          })
-            .then((res) => {
-              if (res.status === 200) {
-                return res.json();
-              } else {
-                // 서버에서 에러코드 전송 시 실행 코드
-              }
-            })
-            .then(() => {
-              // 성공 시 실행 코드
-            })
-            .catch((error) => {
-              // 네트워크 문제로 실패 시 실행 코드
-              console.error(error);
+        onClick={async () => {
+          try {
+            const res = await fetch('/api/post/delete', {
+              method: 'DELETE',
+              body: JSON.stringify({ id }),
             });
+
+            if (res.status === 200) {
+              // 성공 시 실행 코드
+              const data = await res.json();
+            } else {
+              // 서버에서 에러코드 전송 시 실행 코드
+              const data = await res.json();
+              throw new Error(data.message);
+            }
+          } catch (error) {
+            // 네트워크 문제로 실패 시 실행 코드
+            alert(error.message);
+          }
         }}
       >
         삭제
-      </button> */}
+      </button>
       <Link href={`/edit/${id}`} className="text-sm text-gray-500">
         수정
       </Link>
--- forum/pages/api/post/delete.js
+++ forum/pages/api/post/delete.js
@@ -1,7 +1,15 @@
 import { connectDB } from '@/util/database';
 import { ObjectId } from 'mongodb';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
 export default async function handler(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+
+  if (!session) {
+    return res.status(401).json({ message: 'Please sign in' });
+  }
+
   if (req.method === 'DELETE') {
     const db = (await connectDB).db('forum');
     const { id } = JSON.parse(req.body);
