766b234703196b74d0e8ae66694e0cd153df5acb
YEZI <yezi.dev@gmail.com>
2024-03-12 18:37:17 +0900

    template init

---
+++ template/app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <header className="flex justify-between p-4 h-14">
+        <img src="/main/header_1.png" alt="logo 1" />
+        <img src="/main/header_2.png" alt="logo 2" />
+      </header>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    </>
+  );
+}
