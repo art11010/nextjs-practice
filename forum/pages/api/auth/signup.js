0f8d60997410f49cb6804e8766f5d371284bbfea
YEZI <yezi.dev@gmail.com>
2024-05-04 12:00:00 +0900

    check email exist

--- forum/pages/api/auth/signup.js
+++ forum/pages/api/auth/signup.js
@@ -4,9 +4,16 @@ import bcrypt from 'bcrypt';
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const db = (await connectDB).db('forum');
+    const isExist = await db.collection('user').findOne({
+      email: req.body.email,
+    });
 
     if (!req.body.email || !req.body.password) {
       return res.status(500).json('email or password is empty');
+    }
+
+    if (isExist) {
+      return res.status(500).json('email already exist');
     }
 
     try {
