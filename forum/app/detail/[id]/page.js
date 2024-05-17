25477efa7ea4bc567ec3530354b72dbaa06ccb45
YEZI <yezi.dev@gmail.com>
2024-05-17 12:00:00 +0900

    comments UI

--- forum/app/detail/[id]/page.js
+++ forum/app/detail/[id]/page.js
@@ -13,8 +13,17 @@ export default async function Detail(props) {
       <div className="flex flex-col gap-1 p-4 text-center bg-black rounded-lg shadow-lg dark:text-black dark:bg-white">
         <h4 className="font-semibold">상세 페이지</h4>
         <div className="my-4 py-4 border-y border-gray-300">
-          <h6>{item?.title}</h6>
+          <h5>{item?.title}</h5>
           <p className="text-sm">{item?.content}</p>
+        </div>
+        <div className="text-left">
+          <div>
+            <h6>댓글</h6>
+          </div>
+          <div>
+            <input type="text" />
+            <button>작성</button>
+          </div>
         </div>
         <Buttons id={item?._id.toString()} />
       </div>
