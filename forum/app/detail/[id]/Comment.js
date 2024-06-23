54cb32716a57452971caba5ce0d9b347cc14e08f
YEZI <yezi.dev@gmail.com>
2024-06-23 12:00:00 +0900

    comment list UI

--- forum/app/detail/[id]/Comment.js
+++ forum/app/detail/[id]/Comment.js
@@ -16,9 +16,7 @@ export default function Comment({ id }) {
 
   return (
     <div className="px-4">
-      <div>
-        <h6>댓글</h6>
-      </div>
+      <h6>댓글</h6>
       <div className="mt-4 flex gap-4">
         <input
           type="text"
@@ -38,6 +36,16 @@ export default function Comment({ id }) {
         >
           작성
         </button>
+      </div>
+      <div className="mt-4 py-4 space-y-4 border-t">
+        {commentList.map((item) => (
+          <p key={item?._id} className="text-sm">
+            {item?.comment}
+            <span className="mt-0.5 block text-xs text-gray-400">
+              written by {item?.author}
+            </span>
+          </p>
+        ))}
       </div>
     </div>
   );
